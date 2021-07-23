9df797774e3a0b9b0d25a86fe9d427e38d5f45f5
antonio63963 <4access@ukr.net>
2021-07-23 19:39:49 +0300

    defind the message

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = require('./token.txt')
+const TOKEN = '1926993187:AAHlWMMBfl7gfE_1GBQzgvUn7RfU2id4sjU';
 
 const bot = new TelegramBot(TOKEN, {
-  polling: true
+  polling: {
+    interval: 300,
+    autoStart: true,
+    params: {
+      timeout: 10
+    }
+  }
 })
 bot.on('message', (msg) => {
-  console.log(msg);
-  bot.sendMessage(msg.chat.id, 'Hello!' + msg.from.first_name)
+  console.log(msg.text);
+  const { id } = msg.chat;
+  const text = msg.text.toLowerCase();
+  if(text == 'help') {
+    bot.sendMessage(id, 'i will help you!')
+      .then(() => console.log('sms has been send'))
+      .catch((error) => console.log(error))
+  }
+  else {
+    bot.sendMessage(id, debuger(msg))
+  }
+  
 })
+function debuger(obj) {
+  return JSON.stringify(obj, null, 4)
+}
