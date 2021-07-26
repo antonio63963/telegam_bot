0210c9c371f88a0335cbc6e2d45e46b7ed1beb94
unknown <shotvideo25@gmail.com>
2021-07-26 17:21:57 +0300

    keyboard taste

--- main.js
+++ main.js
@@ -10,26 +10,62 @@ const bot = new TelegramBot(TOKEN, {
     }
   }
 })
+// bot.on('message', (msg) => {
+//   console.log(msg.text);
+//   const { id } = msg.chat;
+//   const text = msg.text.toLowerCase();
+//   if(text == 'help') {
+//     bot.sendMessage(id, 'i will help you!')
+//       .then(() => console.log('sms has been send'))
+//       .catch((error) => console.log(error))
+//   }
+//   else {
+//     const html = `
+//     <strong>Test title</strong>
+//     <pre>${debuger(msg)}</pre>
+//     `
+//     bot.sendMessage(id, html, {
+//       parse_mode: 'HTML'
+//     })
+//   }
+  
+// })
+// function debuger(obj) {
+//   return JSON.stringify(obj, null, 4)
+// }
 bot.on('message', (msg) => {
-  console.log(msg.text);
-  const { id } = msg.chat;
-  const text = msg.text.toLowerCase();
-  if(text == 'help') {
-    bot.sendMessage(id, 'i will help you!')
-      .then(() => console.log('sms has been send'))
-      .catch((error) => console.log(error))
-  }
-  else {
-    const html = `
-    <strong>Test title</strong>
-    <pre>${debuger(msg)}</pre>
-    `
-    bot.sendMessage(id, html, {
-      parse_mode: 'HTML'
+  const {id} = msg.chat;
+  console.log(msg);
+  if(msg.text == 'Закрыть') {
+    bot.sendMessage(id, 'close keyboard', {
+      reply_markup: {
+        remove_keyboard: true
+      }
+    })
+  } else if(msg.text === 'Ответить') {
+    bot.sendMessage(id, 'my reply', {
+      reply_markup: {
+        force_reply: true
+      }
+    })
+  } else {
+    bot.sendMessage(id, 'keyboard', {
+      reply_markup: {
+        keyboard: [
+          [{
+            text: 'отправить местоположение',
+            request_location:true
+          }],
+          ['Ответить', 'Закрыть'],
+          [{
+            text: 'Отправить контакт',
+            request_contact: true
+          }]
+        ],
+        one_time_keyboard: true
+      }
     })
   }
-  
+
+
 })
-function debuger(obj) {
-  return JSON.stringify(obj, null, 4)
-}
